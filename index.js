b74098e7f9e420ac65fb5b27578f1af674630c55
Abraham <abukip344@gmail.com>
2024-03-23 07:09:18 -0400

    object lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "Jacob Mast",
+    streetAddress: "Roysambu Drive",
+}
+// modifies original object non-destructively
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let modifiedEmployee = {...employee};
+    modifiedEmployee[key] = value;
+    return modifiedEmployee;
+}
+
+//modifies original object destructively
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+//deletes key non-destructively
+function deleteFromEmployeeByKey(employee, key){
+    const cloneEmployee = {...employee}
+    delete cloneEmployee[key];
+    return cloneEmployee;
+}
+
+//deletes key destructively
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
+
+
